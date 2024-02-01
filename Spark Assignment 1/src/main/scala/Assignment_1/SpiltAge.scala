7d039f1e7491a7b28190c9316be0280f49e9cbc7
HSU PEI-HAN <hsu.p@northeastern.edu>
2024-02-01 10:10:28 -0500

    update Q5

--- Spark Assignment 1/src/main/scala/Assignment_1/SpiltAge.scala
+++ Spark Assignment 1/src/main/scala/Assignment_1/SpiltAge.scala
@@ -9,7 +9,23 @@ import org.apache.spark.sql.functions._
 object SpiltAge {
   def spiltAge(df: DataFrame): Unit = {
     val dfAgeGroup = df.withColumn("AgeGroup"
-      ,when(col("Age").between(0, 1), "0-1")
+      ,when(col("Age") >=0 && col("Age")<1, "0-1")
+      .when(col("Age") >=1 && col("Age")<11, "1-10")
+      .when(col("Age") >=11 && col("Age")<21, "11-20")
+      .when(col("Age") >=21 && col("Age")<31, "21-30")
+      .when(col("Age") >=31 && col("Age")<41, "31-40")
+      .when(col("Age") >=41 && col("Age")<51, "41-50")
+      .when(col("Age") >=51 && col("Age")<61, "51-60")
+      .when(col("Age") >=61 && col("Age")<71, "61-70")
+      .when(col("Age") >=71 && col("Age")<81, "71-80")
+      .when(col("Age") >=81, "81+")
+      .otherwise("Unknown"))
+
+    val ageGroupFare = dfAgeGroup.groupBy("AgeGroup").agg(avg("Fare").alias("avgFare")).orderBy("AgeGroup")
+    ageGroupFare.show()
+  }
+}
+       /* .between(0, 1), "0-1")
         .when(col("Age").between(1, 10), "1-10")
         .when(col("Age").between(11, 20), "11-20")
         .when(col("Age").between(21, 30), "21-30")
@@ -17,10 +33,7 @@ object SpiltAge {
         .when(col("Age").between(41, 50), "41-50")
         .when(col("Age").between(51, 60), "51-60")
         .when(col("Age") > 60, "60+")
-        .otherwise("Unknown"))
+        .otherwise("Unknown"))*/
    // dfAgeGroup.show()
 
-    val ageGroupFare = dfAgeGroup.groupBy("AgeGroup").agg(avg("Fare").alias("avgFare")).orderBy("AgeGroup")
-    ageGroupFare.show()
-  }
-}
+
